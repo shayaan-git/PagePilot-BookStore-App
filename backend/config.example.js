508bfe37a6c9d5b9d877860a544d033878bcf1a7
shayaan-git <mshayaan24@gmail.com>
2025-07-27 01:57:07 +0530

    Secure config by ignoring secrets and adding example

---
+++ backend/config.example.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongodbURL = 'YOUR_MONGODB_CONNECTION_STRING_GOES_HERE';
+
--- backend/config.js
+++
@@ -1,4 +0,0 @@
-export const PORT = 5555;
-
-export const mongodbURL = 'mongodb+srv://rootbs:rootbs1@book-store-mern.ywv9pru.mongodb.net/?retryWrites=true&w=majority&appName=Book-Store-MERN'
-
